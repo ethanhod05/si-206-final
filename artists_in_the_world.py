de7f8ccc5cadc714b106aea8da6cab9a4886af84
Ethan Hod <ehod@umich.edu>
2025-04-21 13:57:01 -0400

    final proj

--- artists_in_the_world.py
+++ artists_in_the_world.py
@@ -12,19 +12,10 @@ def get_top_100():
   
     for tag in soup.find_all('h3', class_='c-title'):
         text = tag.get_text(strip=True)
-        if len(text) > 1 and len(text) < 50:  # crude filter to avoid trash
+        if len(text) > 1 and len(text) < 50:  #filter to avoid trash
             artists.append(text)
 
     unique_artists = list(dict.fromkeys(artists))
-
-#create the integer table for the artists on the list 
-#seperate table /// all in the same database
-
-
-
-# c.execute('''SELECT name FROM name_id ''')
-# name_id = c.fetchall()
-#^^^ will give us a list of all the name_ids
     return unique_artists
 
 
